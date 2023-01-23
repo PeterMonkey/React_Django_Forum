e6992d55ee6cc6038a24e9091d0ec4497e31da6d
Pedro Fernandez <pefernandez1408@gmail.com>
2023-01-23 17:59:03 -0400

    ADD: Se crearon los modelos para Blog y Comentarios

--- backend/forum/blogs/models.py
+++ backend/forum/blogs/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+## Modelo para los blogs
+class Blog(models.Model):
+    body = models.CharField(max_length=120)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+## Modelos para los comentarios
+class Comment(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    text = models.CharField(max_length=100)
+    date = models.DateTimeField(auto_now_add=True)
