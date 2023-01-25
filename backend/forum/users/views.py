b0e7c7c32be73ee3615571aeb107527e439a417b
Pedro Fernandez <pefernandez1408@gmail.com>
2023-01-25 12:18:13 -0400

    ADD: Se creo la ruta de login

--- backend/forum/users/views.py
+++ backend/forum/users/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import serializers
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from django.contrib.auth.hashers import make_password
+from rest_framework import status
 
-# Create your views here.
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from .models import User
+from .serializers import UserSerializerWithToken
+
+## Obtener el Token
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data =  super().validate(attrs)
+
+        serializers = UserSerializerWithToken(self.user).data
+
+        for token, user in serializers.items():
+            data[token] = user
+
+        return data
+
+## clase que apunta el ednpoint de login
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
